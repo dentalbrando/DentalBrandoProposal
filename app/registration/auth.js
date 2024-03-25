d3368adca5b266783de8830ac06bd560a92c619d
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-25 11:46:35 +0500

    a

--- app/registration/auth.js
+++ app/registration/auth.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 import Cookies from "js-cookie";
 
 export function createToken(userData) {
-  console.log("createToken");
   try {
     let securityKey = "securityKey";
     return jwt.sign(userData, securityKey, { expiresIn: "1h" });
@@ -12,23 +11,19 @@ export function createToken(userData) {
 }
 
 export function verifyToken(token) {
-  console.log("verifyToken");
   let securityKey = "securityKey";
   try {
     jwt.verify(token, securityKey);
     return true;
   } catch (err) {
-    console.log(err);
     return false;
   }
 }
 
 export function setCookies(token) {
-  console.log("setCookies");
   Cookies.set("token", token, { expiresIn: "1" });
 }
 
 export function getCookies() {
-  console.log("getCookies");
   return Cookies.get("token");
 }
