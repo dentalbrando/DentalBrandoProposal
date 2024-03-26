3fbbd9845ad9cdf3d48b03725288abca38105660
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-26 09:39:52 +0500

    g

--- components/Registration/Login.jsx
+++ components/Registration/Login.jsx
@@ -49,58 +49,62 @@ function Login(prop) {
   }
 
   return (
-    // <div className="flex flex-col p-5 bg-blue-700 text-xl">
-    <div className="flex flex-col justify-around w-fit p-12 text-xl font-sans w-fit border-l2 border-gray-500 w-[50%] h-[90%] ">
-      <h1
-        className="
-        // bg-gradient-to-r from-blue-400 via-blue-600 to-blue-500 bg-clip-text text-transparent
-        orange_gradient
-        text-6xl pb-10 font-bold"
-      >
-        Login
-      </h1>
+    <div className=" w-5/12 h-full custom-bg*">
+      <div className="flex flex-col justify-center w-fit p-10 text-xl font-sans w-fit borderl-2 border-gray-500 w-full h-[90%] gap-1*">
+        <h1
+          className="
+        // bg-gradient-to-r from-blue-400 via-blue-600 to-blue-500* bg-clip-text text-transparent
+        orange_gradient* 
+        text-5xl pb-5 font-bold "
+        >
+          Login
+        </h1>
 
-      <div className="flex flex-col bg-blue300 h-[50%] justify-center gap-1">
-        <input
-          className="ps-8 pe-16 py-4"
-          placeholder="enter user name"
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          onKeyUp={clickOnEnterPress}
-        />
-        <p className="text-red-500 p-2 px-5 text-lg">{usernameError}</p>
-        <div className="flex w-full justify-end items-center">
+        <div className="flex flex-col bg-blue-300* h-[50%] justify-center gap-1">
           <input
-            name="showPass"
-            placeholder="enter password"
-            className="ps-8 pe-16 py-4 w-full "
-            type={showPassword ? "text" : "password"}
-            onChange={(e) => setPassword(e.target.value)}
+            className="ps-6 pe-12 py-3"
+            placeholder="enter user name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
             onKeyUp={clickOnEnterPress}
           />
-          <div
-            className="absolute float-end mx-5 hover:cursor-pointer text-2xl"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? <FaEye /> : <FaEyeSlash />}
+          <p className="text-red-500 p-2 px-5 text-lg">{usernameError}</p>
+          <div className="flex w-full justify-end items-center">
+            <input
+              name="showPass"
+              placeholder="enter password"
+              className="ps-6 pe-12 py-3 w-full"
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyUp={clickOnEnterPress}
+            />
+            <div
+              className="absolute float-end mx-5 hover:cursor-pointer text-2xl"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEye /> : <FaEyeSlash />}
+            </div>
           </div>
+          <p className="text-red-500 p-2 px-5 text-lg">
+            {!passwordError ? passwordError : loginError}
+          </p>
+          <p className="text-blue-600 hover:underline px-3 text-xl">
+            forgot password
+          </p>
         </div>
-        <p className="text-red-500 p-2 px-5 text-lg">
-          {!passwordError ? passwordError : loginError}
-        </p>
-        <p className="text-blue-600 hover:underline px-3 text-xl">
-          forgot password
-        </p>
+
+        <button
+          className={`${
+            loading
+              ? "custom-bg"
+              : // : "button"
+                "bg-gradient-to-r from-blue-400 via-blue-600 to-blue-500 hover:opacity-[0.9]"
+          } text-white mx-0 p-3`}
+          onClick={loginSubmit}
+        >
+          submit
+        </button>
       </div>
-      <button
-        className={`${
-          loading ? "custom-bg" : "button"
-          // : "bg-gradient-to-r from-blue-400 via-blue-600 to-blue-500 hover:opacity-[0.9]"
-        } text-white  mx-0 p-3`}
-        onClick={loginSubmit}
-      >
-        submit
-      </button>
     </div>
   );
 }
--- components/Registration/page.jsx
+++ components/Registration/page.jsx
@@ -1,13 +1,17 @@
 import Login from "@components/Registration/Login";
-import loginSvg from "@public/assets/loginSvg3.svg";
+import loginSvg from "@public/assets/loginSvg2.svg";
 
 function Registration(prop) {
   let { setTokenVerifierTrigger, tokenVerifierTrigger, setUserId } = prop;
   return (
     <div className="w-screen h-screen custom-bg">
       <div className="flex justify-center items-center w-screen h-screen">
-        <div className="flex items-center w-[80%] h-[80%] justify-evenly rounded-2xl overflow-hidden">
-          <img src={loginSvg.src} alt="" className="custom-bg w-[50%] h-[100%]" />
+        <div className="flex items-center w-[80%] h-[80%] rounded-2xl overflow-hidden custom-bg bg-white">
+          <img
+            src={loginSvg.src}
+            alt=""
+            className="w-7/12 h-[100%]"
+          />
           <Login
             setTokenVerifierTrigger={setTokenVerifierTrigger}
             tokenVerifierTrigger={tokenVerifierTrigger}
