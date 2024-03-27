5fbd6d75408d792f45a659eddad25a801075c5a6
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-27 14:01:24 +0500

    s

--- app/registration/connectDb.js
+++ app/registration/connectDb.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export default async () => {
+  console.log(process.env.DATABASE_URL);
+  console.log(process.env.SECURITY_KEY);
   await mongoose.connect(
-    "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    // "mongodb+srv://ahmadrazakhalid110:RM1dpnTg0GoXyy4A@cluster0.4fv7dem.mongodb.net/"
+    // "mongodb://127.0.0.1:27017/"
+    process.env.DATABASE_URL
   );
 };
