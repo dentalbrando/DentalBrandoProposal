adbb58c942a6e7db118fa9db40866415436afcce
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-28 13:09:41 +0500

    a

--- app/api/verifyToken/route.js
+++ app/api/verifyToken/route.js
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export async function GET(req) {
-  let securityKey = JSON.stringify(process.env.SECURITY_KEY);
-  //   console.log(securityKey);
   let token = cookies().get("authToken");
   if (token) {
     token = token.value;
   } else {
     token = "";
   }
-  jwt.verify(token, "securityKey");
+  let securityKey = process.env.SECURITY_KEY;
+  jwt.verify(token, securityKey);
   return NextResponse.json({ msg: "verified" });
 }
