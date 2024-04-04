97293451ca4a8326221f4e9a1ddee0d708bbbed8
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-04 16:00:42 +0500

    internal cahgnes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSelector } from "react-redux";
 import logo from "@public/assets/new-logo.png";
+import Link from "next/link";
 
 const Nav = (props) => {
   const pageNo = useSelector((state) => state.page.formId);
@@ -8,7 +9,9 @@ const Nav = (props) => {
     pageNo != 100 && (
       <>
         <div className="flex justify-between items-center w-full h-[12vh] px-24 border-b-2 border-color custom-bg">
-          <img src={logo.src} alt="" className="w-[18vw]" />
+          <Link href={"/"}>
+            <img src={logo.src} alt="" className="w-[18vw]" />
+          </Link>
           <h1 className="text-main-blue text-[30px] p-5 font-medium">
             {props.navText ? props.navText : "Proposal Manager"}
           </h1>
