b7f65332f6f70f466f2d95a37346aaaec0081d45
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-25 12:57:57 +0500

    bugs

--- app/designing/page.jsx
+++ app/designing/page.jsx
@@ -20,53 +20,53 @@ import Contact from "./proposalPages/Contact";
 function Designing() {
   return (
     <div className="w-full h-fit flex flex-col items-center gap-0">
-      <div className="w-[21cm] h-[1122.519685px]">
+      <div className="w-[21cm] h-[1122.9px]">
         <CoverPage />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <TableOfContent />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <CoverLetter />
       </div>
 
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <AboutUs />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <ProjectObjective />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <Activities />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <MonthlyCalender />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <ContentCreation />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1124px]">
         <Marketing />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <ValuedClient />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <AdvertisementPackages />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <PostDesignPackages />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <MarketingPackages />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <Estimate />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <Terms />
       </div>
-      <div className="w-[8.27in] h-[29.7cm]">
+      <div className="w-[8.27in] h-[1122.9px]">
         <Contact />
       </div>
     </div>
