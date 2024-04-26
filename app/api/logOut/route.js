07c137862100b569b510634561d9b47eef71ab5c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-26 23:56:12 +0500

    fs

--- app/api/logOut/route.js
+++ app/api/logOut/route.js
@@ -1,9 +1,10 @@
 import { removeTokenFromCookies } from "@app/registration/auth";
 
-
 export async function GET(req) {
   // Assuming Response and Response are available here
+  console.log("try");
   const cookie = removeTokenFromCookies();
+  console.log("catch");
   return new Response(null, {
     headers: { "Set-Cookie": cookie },
   });
