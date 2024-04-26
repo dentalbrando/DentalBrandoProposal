41a85e69a8649faff64e7f929ec7af343375e131
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-26 16:02:15 +0500

    kmk

--- app/api/logOut/route.js
+++ app/api/logOut/route.js
@@ -1,8 +1,4 @@
 import { removeTokenFromCookies } from "@app/registration/auth";
-import connectDb from "@app/registration/connectDb";
-import RegistrationModel from "@app/models/registration";
-import { NextResponse } from "next/server";
-import { serialize } from "cookie";
 
 export async function GET(req) {
   return new Response(null, {
--- app/registration/auth.js
+++ app/registration/auth.js
@@ -16,13 +16,15 @@ export function setTokenToCookies(userData) {
   return cookie;
 }
 
+
 export function removeTokenFromCookies() {
-  const cookie = serialize("authToken", null, {
-    maxAge: 3600,
-    expires: new Date(Date.now() + 3600),
-    httpOnly: true,
-    secure: true,
-    sameSite: "lax",
+  const cookie = serialize("authToken", "", {
+    maxAge: -1, // Set maxAge to -1 to expire the cookie immediately
+    expires: new Date(0), // Set expires to Unix epoch (past date)
+    httpOnly: true, // Ensure cookie is only accessible via HTTP requests
+    secure: true, // Only send the cookie over HTTPS connections (if applicable)
+    sameSite: "lax", // Restrict cookie access for cross-site requests
   });
+
   return cookie;
 }
