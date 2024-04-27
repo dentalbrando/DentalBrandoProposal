20b5dd2bdaeab93953d3e8f6d984da835175a0ba
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-27 19:30:43 +0500

    njn

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -51,8 +51,10 @@ const Sidebar = () => {
   async function logout() {
     try {
       console.log("logging out...");
-      await axios.get("/api/logOut");
-      axios.get("/api/logOut2");
+      // await axios.get("/api/logOut");
+      // await axios.get("/api/logOut2");
+      document.cookie =
+        "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax";
     } catch (error) {
       console.error("Error logging out:", error);
     } finally {
