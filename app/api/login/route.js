bb4aab9fa3b0f9f215bbf677c1903dcc4a73efd4
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-27 17:32:39 +0500

    jnjn

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -2,6 +2,7 @@ import { setTokenToCookies } from "@app/registration/auth";
 import connectDb from "@app/registration/connectDb";
 import RegistrationModel from "@app/models/registration";
 import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 export async function POST(req) {
   try {
@@ -13,16 +14,18 @@ export async function POST(req) {
       if (password !== loginData.password) {
         return NextResponse.json({ error: "Incorrect Password" });
       } else {
+        const token = jwt.sign(userData, "securityKey", { expiresIn: "1h" });
         let dataToSend = { msg: "token", userId: loginData._id };
         return new Response(JSON.stringify(dataToSend), {
-          headers: { "Set-Cookie": setTokenToCookies(userData) },
+          // headers: { "Set-Cookie": setTokenToCookies(userData) },
+          headers: { "Set-Cookie": `authToken=${token}; HttpOnly` },
         });
       }
     } else {
       return NextResponse.json({ error: "User not found" });
     }
   } catch (err) {
-    console.log("err: ", err);
+    console.log("err________________________________________: ", err);
     return NextResponse.json({
       error: "can't process your request at the moment",
     });
--- app/registration/auth.js
+++ app/registration/auth.js
@@ -1,16 +1,15 @@
 import jwt from "jsonwebtoken";
 import { serialize } from "cookie";
-import Cookies from "universal-cookie";
 
 export function setTokenToCookies(userData) {
-  const securityKey = process.env.SECURITY_KEY; // Assuming process.env.SECURITY_KEY is set properly
+  const securityKey = process.env.SECURITY_KEY;
   const token = jwt.sign(userData, securityKey, { expiresIn: "1h" });
 
   const cookie = serialize("authToken", token, {
     maxAge: 3600,
-    expires: new Date(Date.now() + 3600 * 1000), // Use milliseconds
+    expires: new Date(Date.now() + 3600 * 1000),
     httpOnly: true,
-    secure: true, // Set to true if not using HTTPS
+    secure: true,
     sameSite: "lax",
   });
 
@@ -22,7 +21,7 @@ export function removeTokenFromCookies() {
     maxAge: -1,
     expires: new Date(),
     httpOnly: true,
-    secure: true, // Set to true if not using HTTPS
+    secure: true,
     sameSite: "lax",
   });
 
