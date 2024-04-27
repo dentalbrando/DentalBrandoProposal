ff310638d593495e76c66dec45e4915e8109a418
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-27 18:31:00 +0500

    fkmk

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -52,7 +52,6 @@ const Sidebar = () => {
     try {
       console.log("logging out...");
       await axios.get("/api/logOut");
-      await axios.get("/api/logOut");
       // Cookies.remove("authToken");
     } catch (error) {
       console.error("Error logging out:", error);
