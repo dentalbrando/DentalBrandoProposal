5f54881fc248ea54e0807a1fb9ab294bde0d9775
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-04-28 21:15:52 +0500

    mkmkmk

--- app/api/logOut/route.js
+++ app/api/logOut/route.js
@@ -1,8 +1,13 @@
-import { removeTokenFromCookies, setTokenToCookies } from "@app/registration/auth";
+import {
+  removeTokenFromCookies,
+  setTokenToCookies,
+} from "@app/registration/auth";
 
 export async function GET(req) {
   const cookie = removeTokenFromCookies();
-
+  let userData = {
+    name: "ahmad",
+  };
   return new Response(null, {
     headers: { "Set-Cookie": setTokenToCookies(userData, 10) },
   });
