16bffc02fabf337e529b40dcef106d287b9c536b
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-05-03 18:32:44 +0500

    pages

--- app/seo/proposalPages/Activities.jsx
+++ app/seo/proposalPages/Activities.jsx
@@ -8,7 +8,164 @@ function Activities(prop) {
         <div className="flex-end w-full h-[15%] absolute top-0 overflow-hidden ">
           <img src={seo3.src} className="h-full" />
         </div>
-        Activities{" "}
+
+        <div className="flex w-full h-[50%] absolute top-0 overflow-hidden">
+          <div className="absolute w-[87%] h-[50%] right-[6.5%] top-[120px] z-[200] bg-red-90">
+            <div className="flex flex-col justify-center w-full leading-none">
+              <span className="text-[#00A2FF] text-[44px] font-[600]">SEO</span>
+              <span className="text-[#00A2FF] text-[44px] font-[600]">
+                Activities
+              </span>
+              <div className="w-[70px] h-[5px] bg-yellow-400 my-3"></div>
+            </div>
+            <div className="flex flex-col justify-center w-full leading-snug">
+              <span className="text-[white text-[15px] font-[400] text-justify">
+                Our Search Engine Optimization services encompass the detailed
+                project lifecycle broken down into smaller steps. We optimize
+                the online brand presence of your business. We have listed Below
+                are the detailed features of each SEO activity. They combine to
+                craft a strong search engine optimized reputation for your
+                business. Get in touch with us to elevate your search presence!
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-[87%] h-[57%] absolute bottom-[12%] overflow-hidde bg-red-30">
+          <div className="flex flex-col justify-center items-center w-[100%] h-[100%] relative bg-yellow-30">
+            <div className="flex flex-col w-[100%] h-[33%] absolute top-[0%]  z-[0]">
+              <div className="flex w-[100%] h-[37%] flex justify-between">
+                <div className="flex w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px] bg-[#00A2FF] rounded-full flex justify-center items-center text-[35px] text-white font-bold">
+                    01
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+
+                <div className="flex flex-row-reverse w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px] bg-[rgb(255,213,0)] rounded-full flex justify-center items-center text-[35px] text-white font-bold">
+                    02
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+              </div>
+              <div className="flex-between w-[100%] h-[63%] ">
+                <div
+                  className="flex flex-col relative left-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    Keyword Research and Optimization
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    Extensive Keyword Research to find relevant keywords and
+                    optimize your content accordingly.
+                  </span>
+                </div>{" "}
+                <div
+                  className="flex flex-col relative right-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    Content Strategy
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    A tailored content strategy is focused on creating engaging,
+                    relevant, and optimized content.{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col w-[100%] h-[33%] absolute top-[33%]  z-[0]">
+              <div className="flex w-[100%] h-[37%] flex justify-between">
+                <div className="flex w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px] bg-[rgb(255,213,0)] rounded-full flex justify-center items-center text-[35px] text-white font-bold">
+                    03
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+
+                <div className="flex flex-row-reverse w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px]  rounded-full  bg-[#00A2FF] flex justify-center items-center text-[35px] text-white font-bold">
+                    04
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+              </div>
+              <div className="flex-between w-[100%] h-[63%] ">
+                <div
+                  className="flex flex-col relative left-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    On-Page SEO{" "}
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    Implements best practices for on-page optimization to
+                    enhance your website authority on the web.{" "}
+                  </span>
+                </div>{" "}
+                <div
+                  className="flex flex-col relative right-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    Technical SEO Audit{" "}
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    Detailed analysis of your website's technical structure to
+                    enhance your search engine visibility.{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col w-[100%] h-[33%] absolute top-[66%]  z-[0]">
+              <div className="flex w-[100%] h-[37%] flex justify-between">
+                <div className="flex w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px]   bg-[#00A2FF] rounded-full flex justify-center items-center text-[35px] text-white font-bold">
+                    05
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+
+                <div className="flex flex-row-reverse w-[45%] h-[100%]  flex justify-start items-center">
+                  <div className="flex w-[80px] h-[80px]  bg-[rgb(255,213,0)] rounded-full flex justify-center items-center text-[35px] text-white font-bold">
+                    06
+                  </div>
+                  <div className="flex w-[70%] h-[1px] bg-black"></div>
+                </div>
+              </div>
+              <div className="flex-between w-[100%] h-[63%] ">
+                <div
+                  className="flex flex-col relative left-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    Off-Page SEO and Link Building{" "}
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    Acquire high-quality backlinks from authoritative websites
+                    to enhance your site's credibility.{" "}
+                  </span>
+                </div>{" "}
+                <div
+                  className="flex flex-col relative right-[75px] h-[100%]  text-justify"
+                  style={{ width: "calc(45% - 75px" }}
+                >
+                  <span className="text-[19px] font-[600] text-[#00A2FF] text-start w-[80%]">
+                    Performance Tracking and Reporting{" "}
+                  </span>
+                  <span className="text-[15px] font-[400]">
+                    Regular performance reports detailing key metrics enable you
+                    to track and optimize SEO progress.{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex w-[10px] h-[100%] bg-[rgb(0,160,255)] z-[20]"></div>
+          </div>
+        </div>
+
         <div className="absolute bottom-0 h-[110px] w-[100%] overflow-hidden bg-red-40">
           <div className="flex justify-between items-center w-full px-14 h-full absolute top-[18%] left-0 z-[50]">
             <span className="text-gray-700 text-[15px] font-[500]">
