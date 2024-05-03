4b30da41552258ef26e02c017d7505966ec70eaa
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-05-03 18:55:23 +0500

    pages

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,11 +9,11 @@ const Nav = (props) => {
     pageNo != 100 && (
       <>
         <div className="flex justify-start items-center w-full h-[12vh] border-b-2 border-color custom-b relative">
-          <Link href={"/"} className="w-[20vw] ps-7">
+          <Link href={"/"} className="w-[20vw] ps-7 z-[10]">
             <img src={logo.src} alt="" className="w-[18vw]" />
           </Link>
           <h1
-            className={`text-main-blue text-[30px] font-medium 
+            className={`text-main-blue text-[30px] font-medium z-[0]
           ${props.main ? "w-full" : "w-[80vw]"}
           w-[78vw]
           flex justify-center absolute right-0`}
