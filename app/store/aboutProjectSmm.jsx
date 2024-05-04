8fb11cf150498e2a91113820ea7051f2935a51b3
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-05-04 15:26:48 +0500

    po smm

--- app/store/aboutProjectSmm.jsx
+++ app/store/aboutProjectSmm.jsx
@@ -1,8 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  aboutProject:
-    "Our specialized expertise in Social Media Marketing will enhance your brand’s social media presence. We aim to bring you business by targeting your sophisticated audience interested in the residential apartments of Vista Cornard Tower. We will involve multiples social media platforms, like Facebook, Instagram, LinkedIn, Twitter, Pinterest etc, for marketing your mall and residential apartments. We believe that powerful content creation is the backbone of marketing success, so we will create engaging content that showcases the luxury mall, amenities, and lifestyle offered by you.",
+  aboutProject: "",
 };
 
 const AboutProjectSmm = createSlice({
