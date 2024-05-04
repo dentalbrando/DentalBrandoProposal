cbc3b397804d128e196c36bd8fe074827b05ccf4
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-05-04 12:16:36 +0500

    d

--- app/social-media/forms/CoverLetterForm.jsx
+++ app/social-media/forms/CoverLetterForm.jsx
@@ -1,15 +1,36 @@
 "use client";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { setClientName, setLetterText } from "@app/store/CoverLetterSmm";
 import Select from "react-select";
 const CoverLetterForm = () => {
   const dispatch = useDispatch();
   let coverLetterSmm = useSelector((state) => state.cover_letterSmm);
   const [clientName, setclientName] = useState(coverLetterSmm.clientName);
-  const [letterText, setletterText] = useState(coverLetterSmm.letterText);
   const [letterType, setletterType] = useState("Social Media");
 
+  if (coverLetterSmm.letterText === "") {
+    var tempLetterText = `We are writing to express our interest in your web development project. As a company with extensive experience in web development and a track record of delivering exceptional results for our clients, we are confident that we would be the ideal partner for your project.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+At Soft Enterprise, we understand that your website is often the first point of contact between your business and your customers. As such, we are committed to migrate your website to the sharepoint.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Our team of experienced web developers, designers, and project manager work closely with clients to understand their specific needs and requirements. We have expertise in a wide range of technologies including Wordpress, Laravel, Angular, PHP, Share Point, Vue.Js, React, Shopify, Python and more. We are always exploring new and innovative approaches to web development.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+At every stage of the project, we prioritize communication and collaboration to ensure that you are fully involved in the process and the final product meets your expectations. We are dedicated to delivering projects on time and within budget, and we offer going support and maintenance to ensure that your website continues to perform at its best.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Thank you for considering Soft Enterprise for your web development needs. We are excited at the prospect of working with you and creating a website that will help your business succeed.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Sincerely,`;
+  } else {
+    var tempLetterText = coverLetterSmm.letterText;
+  }
+
+  const [letterText, setletterText] = useState(tempLetterText);
   const handleSave = () => {
     dispatch(setLetterText(letterText));
     dispatch(setClientName(clientName));
@@ -71,8 +92,12 @@ const CoverLetterForm = () => {
                 rows="5"
                 className={`text-gray-900  text-[14px] font-normal p-2 rounded-md border-2 border-color outline-none rounded-md font-normal border-2 border-color outline-none w-[80%]`}
                 onChange={(e) => setletterText(e.target.value)}
-                value={coverLetterSmm.letterText}
-              ></textarea>
+              >
+                {letterText
+                  .trim()
+                  .replace(/<p\s*[^>]*>/g, "\n")
+                  .replace(/<\/p>/g, "")}
+              </textarea>
             </div>
           </div>
         </div>
--- app/social-media/proposalPages/CoverLetter.jsx
+++ app/social-media/proposalPages/CoverLetter.jsx
@@ -4,9 +4,50 @@ import sign from "@public/assets/socialMedia/sign.fcbecd86.jpg";
 import { useSelector } from "react-redux";
 function CoverLetter(prop) {
   let coverLetterSmm = useSelector((state) => state.cover_letterSmm);
+  if (coverLetterSmm.letterText === "") {
+    var tempLetterText = `We are writing to express our interest in your web development project. As a company with extensive experience in web development and a track record of delivering exceptional results for our clients, we are confident that we would be the ideal partner for your project.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+At Soft Enterprise, we understand that your website is often the first point of contact between your business and your customers. As such, we are committed to migrate your website to the sharepoint.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Our team of experienced web developers, designers, and project manager work closely with clients to understand their specific needs and requirements. We have expertise in a wide range of technologies including Wordpress, Laravel, Angular, PHP, Share Point, Vue.Js, React, Shopify, Python and more. We are always exploring new and innovative approaches to web development.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+At every stage of the project, we prioritize communication and collaboration to ensure that you are fully involved in the process and the final product meets your expectations. We are dedicated to delivering projects on time and within budget, and we offer going support and maintenance to ensure that your website continues to perform at its best.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Thank you for considering Soft Enterprise for your web development needs. We are excited at the prospect of working with you and creating a website that will help your business succeed.
+<p class = 'py-1'></p>
+<p class = 'py-1'></p>
+Sincerely,`;
+  } else {
+    var tempLetterText = coverLetterSmm.letterText;
+  }
+  let content = tempLetterText
+    .split("<p class = 'py-1'></p>")
+    .map((line) => {
+      if (line.trim().startsWith(">")) {
+        const ticked = `<div class="flex">
+                    
+                    <div class="flex items-center justify-center">
 
-  // Apply line breaks
-  // coverLetterText = coverLetterText.replace(/<br \/>/g, "\n");
+                        <img src="${tick.src}" alt="" width="10px" />
+                    </div>
+                    <div class="px-2" >
+                        <p>
+                            ${line.substring(line.indexOf(">") + 1)}
+                        </p>
+                    </div>
+                </div>`;
+
+        return ticked;
+      }
+
+      line = line + "<p class = 'py-1'></p>";
+      return line;
+    })
+    .join("");
 
   return (
     <div className="w-[8.27in] h-[1123px] bg-white shadow-lg">
@@ -26,8 +67,12 @@ function CoverLetter(prop) {
             <span className="text-[24px] font-[700] underline">
               Dear {coverLetterSmm.clientName}
             </span>
+            <div
+              class="Part-one font-[400] text-[12pt] leading-6 pt-2"
+              dangerouslySetInnerHTML={{ __html: content }}
+            ></div>
 
-            <p className="text-[15px] font-[400] mt-1 leading-normal">
+            {/* <p className="text-[15px] font-[400] mt-1 leading-normal">
               {coverLetterSmm.letterText.split("\n").map((paragraph, index) => (
                 <p
                   key={index}
@@ -36,7 +81,7 @@ function CoverLetter(prop) {
                   {paragraph}
                 </p>
               ))}
-            </p>
+            </p> */}
           </div>
           <div className="flex flex-col justify-center w-full mt-1">
             <div className="w-[200px] bg-red-300 ms-[-13px]">
