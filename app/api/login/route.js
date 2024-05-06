688172f53c20d8e202461ef709b82ab16418bf34
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-05-06 11:36:15 +0500

    key

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 
 export async function POST(req) {
   let securityKey = process.env.SECURITY_KEY;
-  console.log(securityKey);
+  // console.log(securityKey);
   try {
     let { username, password } = await req.json();
     let userData = { username, password };
--- app/api/verifyToken/route.js
+++ app/api/verifyToken/route.js
@@ -4,8 +4,7 @@ import jwt from "jsonwebtoken";
 
 export async function GET(req) {
   let securityKey = process.env.SECURITY_KEY;
-  console.log(securityKey);
-
+  // console.log(securityKey);
   let token = cookies().get("authToken");
   if (token) {
     token = token.value;
