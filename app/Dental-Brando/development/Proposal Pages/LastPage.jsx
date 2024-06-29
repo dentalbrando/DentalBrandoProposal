1ac3ca13755a188a9538338703e2a2231ffe4a7a
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-06-29 18:01:03 +0500

    dsd

--- app/Dental-Brando/development/Proposal Pages/LastPage.jsx
+++ app/Dental-Brando/development/Proposal Pages/LastPage.jsx
@@ -35,10 +35,15 @@ const LastPage = () => {
             </div>
             <div class="w-fit">
               <img
-                class="w-[110px] absolute bottom-[70px] mx-10"
+                class="w-[110px] absolute bottom-[70px] mx-10 z-0"
                 src={svg1.src}
                 alt=""
               />
+              <div class="w-[110px] h-[110px] absolute bottom-[70px] mx-10  flex justify-center items-center"> 
+              <div class="w-[40px] h-[30px] bg-white z-30 "> 
+
+              </div>
+              </div>
             </div>
             <div class="flex gap-16  absolute bottom-[70px] right-10">
               <div class="  flex flex-col items-center justify-center">
