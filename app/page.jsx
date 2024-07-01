db26f69dd75bb9b85a641ca3e0bfa0bd5fbaa91a
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-01 16:35:17 +0500

    ds

--- app/page.jsx
+++ app/page.jsx
@@ -13,12 +13,12 @@ import axios from "axios";
 import SaveProposaltoDb from "@components/saveProposaltoDb";
 import Link from "next/link";
 import Loader from "@components/Loader";
-import layer3 from "@public/assets/Layer_1 (1).png";
-import layer2 from "@public/assets/Layer_1 (2).png";
-import layer1 from "@public/assets/Layer_1.png";
-import frame1 from "@public/assets/Frame 4.png";
-import frame2 from "@public/assets/Frame 5.png";
-import frame3 from "@public/assets/Frame 6.png";
+import layer3 from "@public/assets/Layer_1 (3).svg";
+import layer2 from "@public/assets/Layer_1 (2).svg";
+import layer1 from "@public/assets/Layer_1 (1).svg";
+import frame1 from "@public/assets/Frame 4.svg";
+import frame2 from "@public/assets/Frame 5.svg";
+import frame3 from "@public/assets/Frame 6.svg";
 import StoreProvider from "./store/StoreProvider";
 const familyData = {
   name: "John",
@@ -100,37 +100,73 @@ const Home = () => {
       ) : isVerified ? (
         <>
           <Nav main={true} />
-         
+
           <div className="w-[95%] 2xl:w-[87%] h-full mx-auto pt-20 ">
             <div className="w-full h-full flex flex-col gap-2 md:gap-5 z-100">
-              <div className="w-full h-fit linkDiv rounded-[10px] flex flex-row justify-center items-center gap-2 md:gap-5 xl:gap-20 overflow-auto">
-                <Link href={"/development"} className="w-[25%] h-[20%]">
-                  <div className="w-[100%] h-[100%] relative">
-                    <div className="w-[100%] h-[100%]">
-                      <img src={frame3.src} className="w-[100%] h-[100%]" />
-                    </div>
-                    <div className="h-fit w-[50%] absolute bottom-0 right-0">
-                      <img src={layer2.src} />
+              <div className="w-full h-full linkDiv rounded-[10px] flex flex-row justify-center items-center gap-2 md:gap-5 xl:gap-20 overflow-auto">
+                <Link
+                  href={"/development"}
+                  className="w-[350px] h-[450px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                >
+                  <div className="w-[100%] h-[100%] relative bg-yellow-00">
+                    <div className="w-full h-full flex flex-col justify-between">
+                      <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
+                        <div className="w-[170px] h-[170px] bg-blue-40">
+                          <img src={frame3.src} className="w-[100%] h-[100%]" />
+                        </div>
+                      </div>
+                      <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center">
+                          Proposal For Development
+                        </h1>
+                        <div className="h-fit w-[50%] absolute bottom-0 right-0">
+                          <img src={layer1.src} />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </Link>
-                <Link href={"/seo"} className="w-[25%] h-[20%]">
-                  <div className="w-[100%] h-[100%] relative">
-                    <div className="w-[100%] h-[100%]">
-                      <img src={frame1.src} className="w-[100%] h-[100%]" />
-                    </div>
-                    <div className="h-fit w-[50%] absolute bottom-0 right-0">
-                      <img src={layer1.src} />
+                <Link
+                  href={"/seo"}
+                  className="w-[350px] h-[450px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                >
+                  <div className="w-[100%] h-[100%] relative bg-yellow-00">
+                    <div className="w-full h-full flex flex-col justify-between">
+                      <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
+                        <div className="w-[170px] h-[170px] bg-blue-40">
+                          <img src={frame2.src} className="w-[100%] h-[100%]" />
+                        </div>
+                      </div>
+                      <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center">
+                          Proposal For SEO
+                        </h1>
+                        <div className="h-fit w-[50%] absolute bottom-0 right-0">
+                          <img src={layer2.src} />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </Link>
-                <Link href={"/social-media"} className="w-[25%] h-[20%]">
-                  <div className="w-[100%] h-[100%] relative">
-                    <div className="w-[100%] h-[100%]">
-                      <img src={frame2.src} className="w-[100%] h-[100%]" />
-                    </div>
-                    <div className="h-fit w-[50%] absolute bottom-0 right-0">
-                      <img src={layer3.src} />
+                <Link
+                  href={"/social-media"}
+                  className="w-[350px] h-[450px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                >
+                  <div className="w-[100%] h-[100%] relative bg-yellow-00">
+                    <div className="w-full h-full flex flex-col justify-between">
+                      <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
+                        <div className="w-[170px] h-[170px] bg-blue-40">
+                          <img src={frame1.src} className="w-[100%] h-[100%]" />
+                        </div>
+                      </div>
+                      <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center">
+                          Proposal For Social Media
+                        </h1>
+                        <div className="h-fit w-[50%] absolute bottom-0 right-0">
+                          <img src={layer3.src} />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </Link>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSelector } from "react-redux";
-import logo from "@public/assets/new-logo.png";
+import logo from "@public/assets/DB Logo-15.svg";
 import Link from "next/link";
 
 const Nav = (props) => {
@@ -9,8 +9,8 @@ const Nav = (props) => {
     
       <>
         <div className="flex justify-start items-center w-full h-[12vh] border-b-2 border-color custom-b relative">
-          <Link href={"/"} className="w-[20vw] ps-7 z-[10]">
-            <img src={logo.src} alt="" className="w-[18vw]" />
+          <Link href={"/"} className="w-[20vw] flex justify-end items-center z-[10]">
+            <img src={logo.src} alt="" className="w-[120px]" />
           </Link>
           <h1
             className={`text-main-blue text-[30px] font-medium z-[0]
