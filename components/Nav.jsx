bad103c84a80bbce0652cd29a7b58d27b5375dc6
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-02 11:04:43 +0500

    kmkm

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = (props) => {
       <>
         <div className="flex justify-start items-center w-full h-[12vh] border-b-2 border-color custom-b relative">
           <div  className="w-[20vw] flex justify-end items-center z-[10]">
-          <Link href={"/"} className="w-[200px] flex justify-end items-center z-[10]">
+          <Link href={"/"} className="w-[220px] flex justify-end items-center z-[10]">
             <img src={logo.src} alt="" className="w-full" />
           </Link>
           </div>
