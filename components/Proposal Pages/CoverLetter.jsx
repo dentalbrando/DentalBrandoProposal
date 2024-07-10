cc19a0523756f95ff59472f0b221ed4e500921ec
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-10 14:55:30 +0500

    njnjnj

--- components/Proposal Pages/CoverLetter.jsx
+++ components/Proposal Pages/CoverLetter.jsx
@@ -44,41 +44,44 @@ Sincerely,`;
     .join("");
   return (
     <div className="offer flex w-[8.27in] h-[1122.6px] shadow-lg bg-white">
-      <div className=" flex w-full">
-        <div className="flex">
-          <div className="ml-3 w-[3px] h-full bg-[#093A5B] "></div>
-          <div className="line ml-3 mr-1 h-full w-[3px]  bg-[#03A0D7] "></div>
-          <div className="mt-[50px] h-[90%] relative w-full ">
-            <svg
-              className="transform rotate-90 relative  top-0 "
-              width="64"
-              height="64"
-              viewBox="0 0 256 256"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="#093A5B"
-                d="m221.76 69.66l-88-48.18a12 12 0 0 0-11.52 0l-88 48.18A12 12 0 0 0 28 80.18v95.64a12 12 0 0 0 6.24 10.52l88 48.18a11.95 11.95 0 0 0 11.52 0l88-48.18a12 12 0 0 0 6.24-10.52V80.18a12 12 0 0 0-6.24-10.52ZM220 175.82a4 4 0 0 1-2.08 3.5l-88 48.18a4 4 0 0 1-3.84 0l-88-48.18a4 4 0 0 1-2.08-3.5V80.18a4 4 0 0 1 2.08-3.5l88-48.18a3.94 3.94 0 0 1 3.84 0l88 48.18a4 4 0 0 1 2.08 3.5Z"
-              />
-            </svg>
-            <h6 className="absolute ml-[25px] top-5 text-[#03A0D7] text-[12pt]">
-              {pageNumber}
-            </h6>
-          </div>
+      <div className=" flex w-full relative bg-green-90">
+        <div className="flex bg-orange-00 h-full w-full absolute left-0 top-0 z-10">
+          <div className="ml-2 w-[3px] h-full bg-[#093A5B] "></div>
+          <div className="line ml-1 mr-1 h-full w-[3px]  bg-[#03A0D7] "></div>
         </div>
 
-        <div className=" w-[85%]">
-          <h3 className="mt-[60px] font-[700] text-[9pt] text-[#03A0D7]">
-            Dental Brando
-          </h3>
+        <div className=" w-[90%] mx-auto bg-green-00 relative">
+          <div class="flex  print:h-[7.3rem] bg-green-40">
+            <div class="mt-[50px] flex relative  pb-0">
+              <svg
+                class="transform rotate-90 relative ml-[-5px]  top-0 "
+                width="64"
+                height="64"
+                viewBox="0 0 256 256"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="#093A5B"
+                  d="m221.76 69.66l-88-48.18a12 12 0 0 0-11.52 0l-88 48.18A12 12 0 0 0 28 80.18v95.64a12 12 0 0 0 6.24 10.52l88 48.18a11.95 11.95 0 0 0 11.52 0l88-48.18a12 12 0 0 0 6.24-10.52V80.18a12 12 0 0 0-6.24-10.52ZM220 175.82a4 4 0 0 1-2.08 3.5l-88 48.18a4 4 0 0 1-3.84 0l-88-48.18a4 4 0 0 1-2.08-3.5V80.18a4 4 0 0 1 2.08-3.5l88-48.18a3.94 3.94 0 0 1 3.84 0l88 48.18a4 4 0 0 1 2.08 3.5Z"
+                />
+              </svg>
+              <h6 class="absolute ml-[1.4rem] top-5 text-[#03A0D7]  text-[12pt]">
+                {pageNumber}
+              </h6>
+            </div>
+            <div class="mt-[65px]">
+              <h3 class="font-[700] text-[9pt] text-[#03A0D7]">
+                Dental Brando
+              </h3>
 
-          <h3 className=" font-[700] text-[9pt] text-[#03A0D7]">
-            Business Proposal
-          </h3>
+              <h3 class=" font-[700]  text-[9pt] text-[#03A0D7]">
+                Business Proposal
+              </h3>
 
-          <div className=" mt-2 w-[6.7in] h-0.5 bg-[#093A5B] "></div>
-
-          <div className="mt-[70px]">
+              <div class=" mt-1  w-[6.5in] h-0.5 bg-[#093A5B] "></div>
+            </div>
+          </div>
+          <div className="mt-[70px] bg-pink-70">
             <h1
               className="font-[700] text-[25pt] text-[#093A5B] leading-9"
               style={{ fontFamily: "'Montserrat', sans-serif" }}
@@ -96,69 +99,10 @@ Sincerely,`;
               </span>
             </p>
           </div>
-          <div className=" font-[400] text-[12pt] leading-5">
-            {/* {cover_letter.letterType === 'web_dev' ?
-                            <>
-                                <div className="Part-one  mt-7 mr-2 ">
-                                    <p>
-                                        We are writing to express our interest in your web development project. As a company
-                                        with extensive experience in web development and a track record of delivering
-                                        exceptional results for our clients, we are confident that we would be the ideal partner
-                                        for your project.
-                                    </p>
 
-                                </div>
-                                <div className="part-two mt-4  mr-6 ">
-
-                                    <p>
-                                        At Dental Brando, we understand that your website is often the first point of contact
-                                        between your business and your customers. As such, we are committed to migrate
-                                        your website to the sharepoint.
-                                    </p>
-                                </div>
-                                <div className="part-three mt-4 mr-8">
-
-                                    <p>
-                                        Our team of experienced web developers, designers, and project manager work
-                                        closely with clients to understand their specific needs and requirements. We have
-                                        expertise in a wide range of technologies including Wordpress, Laravel, Angular, PHP,
-                                        Share Point, Vue.Js, React, Shopify, Python and more. We are always exploring new
-                                        and innovative approaches to web development.
-                                    </p>
-                                </div>
-                                <div className="part-four mt-4 mr-6 ">
-
-                                    <p>
-                                        At every stage of the project, we prioritize communication and collaboration to ensure
-                                        that you are fully involved in the process and the final product meets your
-                                        expectations. We are dedicated to delivering projects on time and within budget, and
-                                        we offer going support and maintenance to ensure that your website continues to
-                                        perform at its best.
-                                    </p>
-                                </div>
-                                <div className="part-five mt-4 ">
-
-                                    <p>
-                                        Thank you for considering Dental Brando for your web development needs. We are
-                                        excited at the prospect of working with you and creating a website that will help your
-                                        business succeed.
-                                    </p>
-                                </div>
-                                <div className="part-six mt-4 ">
-
-                                    <p>
-                                        Sincerely,
-                                    </p>
-                                </div>
-                            </>
-                            :
-                            cover_letter.letterType === 'web_design' ?
-                                <>
-                                    
-                                </>
-                                : null} */}
+          <div className=" font-[400] text-[12pt] leading-5 bg-red-00">
             <div
-              class="Part-one font-[400] text-[12pt] leading-6 pt-2"
+              class="Part-one font-[400] text-[12pt] leading-6 pt-2 bg-yellow-00"
               dangerouslySetInnerHTML={{ __html: content }}
             ></div>
             <div>
