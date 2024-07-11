bd07ec8e373608138d925faaf8781e76169e325c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-11 10:43:44 +0500

    gnekngk

--- components/Proposal Pages/LastPage.jsx
+++ components/Proposal Pages/LastPage.jsx
@@ -1,8 +1,10 @@
 import svg1 from "@public/assets/LastPageImgs/last page-01.png";
 import svg2 from "@public/assets/LastPageImgs/last page-02.svg";
 import svg3 from "@public/assets/LastPageImgs/last page-03.svg";
+import svg4 from "@public/assets/LastPageImgs/icons8-website-symbol-50.png";
 import bgImg from "@public/assets/DB Web Development Proposal-12.svg";
 import logo from "@public/assets/DB Logo-15.svg";
+import { FaMap, FaSitemap, FaWeebly, FaWordpress } from "react-icons/fa";
 
 const LastPage = () => {
   return (
@@ -27,34 +29,38 @@ const LastPage = () => {
                 src={svg1.src}
                 alt=""
               />
-            </div> */}
-            <div class="flex gap-4  absolute bottom-[150px] left-10">
-              <div class="mt-3  flex flex-col items-center justify-center">
+              </div> */}
+
+            <div class=" mx-10  absolute bottom-[230px]">
+              <h1
+                class="font-[700]  text-[25pt] text-[#093A5B] leading-10"
+                style={{ fontFamily: "'Montserrat', sans-serif" }}
+              >
+                CONTACT US{" "}
+              </h1>
+              <div class=" w-[95px]  h-2 bg-[#03A0D7] "></div>
+            </div>
+
+            <div class="flex gap-x-4 absolute bottom-[90px] left-10 bg-red-40 flex-col w-[500px]">
+              <div class="mt-3 flex flex-co items-start justify-start gap-2 w-fit">
                 <div class="w-8 h-8 bg-[#093A5B] rounded-full flex items-center justify-center">
                   <img class="w-4" src={svg2.src} alt="" />
                 </div>
-                <p class="font-[500] text-[8pt] mt-2">+92 (340) 0084643</p>
+                <p class="font-[500] text-[10pt] mt-2">+92 (340) 0084643</p>
               </div>
 
-              <div class="mt-3 flex flex-col  items-center justify-center">
+              <div class="mt-3 flex flex-co items-start justify-start gap-2 w-fit">
+                <div class="w-8 h-8 bg-[#093A5B] rounded-full flex items-center justify-center">
+                  <img class="w-5" src={svg4.src} alt="" />
+                </div>
+                <p class="font-[500] text-[10pt] mt-2">www.dentalbrando.com</p>
+              </div>
+
+              <div class="mt-3 flex flex-co items-start justify-start gap-2 w-fit">
                 <div class="w-8 h-8 bg-[#093A5B] rounded-full flex items-center justify-center">
                   <img class="w-4" src={svg3.src} alt="" />
                 </div>
-
-                <p class="font-[500] text-[8pt] mt-2">
-                  {/* <span>Website: <br/></span> */}
-                  sales@dentalbrando.com
-                </p>
-              </div>
-              <div class="mt-3 flex flex-col  items-center justify-center">
-                <div class="w-8 h-8 bg-[#093A5B] rounded-full flex items-center justify-center">
-                  <img class="w-4" src={svg3.src} alt="" />
-                </div>
-
-                <p class="font-[500] text-[8pt] mt-2">
-                  {/* <span>Email: </span> */}
-                  sales@dentalbrando.com
-                </p>
+                <p class="font-[500] text-[10pt] mt-2">sales@dentalbrando.com</p>
               </div>
             </div>
 
@@ -75,16 +81,6 @@ const LastPage = () => {
                 again for considering us. We hope that you now understand how
                 Dental Brando can help dentists like you grow online.
               </p>
-            </div>
-
-            <div class=" mx-10  absolute bottom-[250px]">
-              <h1
-                class="font-[700]  text-[25pt] text-[#093A5B] leading-10"
-                style={{ fontFamily: "'Montserrat', sans-serif" }}
-              >
-                CONTACT US{" "}
-              </h1>
-              <div class=" w-[95px]  h-2 bg-[#03A0D7] "></div>
             </div>
 
             <div class="mt-1 flex items-center justify-center w-[8.27in] bg-blue- gap-2 px-11 absolute text-center bottom-5"></div>
