92e6f81eb983e7e13e5ce1c5ad0595cceff8fb4c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-12 18:34:37 +0500

    mkk

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -145,7 +145,7 @@ const Sidebar = () => {
                       { id: "6", content: "Our Process", checked: true },
                       { id: "7", content: "Why Dental Brando", checked: true },
                       { id: "8", content: "Choose Your Plan", checked: true },
-                      { id: "9", content: "Thrive with Us", checked: true },
+                      { id: "9", content: "Thrive With Us", checked: true },
                       {
                         id: "10",
                         content: "Terms & Conditions",
