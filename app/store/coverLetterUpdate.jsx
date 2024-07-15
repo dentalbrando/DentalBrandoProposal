62702f381a3225db2f3b4771a0832eec6c51ea02
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-15 09:54:08 +0500

    fndknj

---
+++ app/store/coverLetterUpdate.jsx
@@ -0,0 +1,29 @@
+// coverLetterSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  letterText: "",
+  clientName: "",
+  letterType: "web_dev",
+};
+
+const coverLetterSlice = createSlice({
+  name: "cover_letter_update",
+  initialState,
+  reducers: {
+    setLetterText: (state, action) => {
+      state.letterText = action.payload;
+    },
+    setClientName: (state, action) => {
+      state.clientName = action.payload;
+    },
+    setLetterType: (state, action) => {
+      state.letterType = action.payload;
+    },
+  },
+});
+
+export const { setLetterText, setClientName, setLetterType } =
+  coverLetterSlice.actions;
+
+export default coverLetterSlice.reducer;
