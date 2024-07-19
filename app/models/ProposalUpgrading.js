1fe710013892f5dffd8ef6fa7828b9e420edfccc
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-19 10:53:46 +0500

    dncjj

--- app/models/ProposalUpgrading.js
+++ app/models/ProposalUpgrading.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
-const ProposalSchema = mongoose.Schema({
+const ProposalUpgradingSchema = mongoose.Schema({
   date: { type: String, required: true },
   aboutYourProject: { type: Object, required: true },
   budget: { type: Object, required: true },
   cover_letter: { type: Object, required: true },
   cover_page: { type: Object, required: true },
   pageSequence: { type: Object, required: true },
-  proposedSitemap: { type: Object, required: true },
-  proposedSitemap2: { type: Object, required: true },
-  proposedSitemapOverview: { type: Object, required: true },
 }
 );
 const ProposalUpgradingModel =
   mongoose.models.ProposalUpgrading ||
-  mongoose.model("ProposalUpgrading", ProposalSchema);
+  mongoose.model("ProposalUpgrading", ProposalUpgradingSchema);
 export default ProposalUpgradingModel;
