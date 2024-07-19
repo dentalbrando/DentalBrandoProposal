fce9a714e221fd6152a38f383bb74400292975dd
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-19 10:09:05 +0500

    xkok

--- app/page.jsx
+++ app/page.jsx
@@ -57,22 +57,22 @@ const Home = () => {
         <>
           <Nav main={true} />
 
-          <div className="w-[95%] 2xl:w-[87%] h-full mx-auto pt-20 ">
-            <div className="w-full h-full flex flex-col gap-2 md:gap-5 z-100 overflow-auto scroll-bar-none">
-              <div className="w-fit h-fit linkDi rounded-3xl flex mx-auto gap-2 md:gap-5 xl:gap-20 items-center  overflow-x-auto leading-10">
+          <div className="w-[95%] 2xl:w-[87%] h-fit mx-auto pt-20 ">
+            <div className="w-full h-[60vh] flex flex-col justify-center items-center bg-green-40  gap-2 md:gap-5 z-100 overflow-auto scroll-bar-none">
+              <div className="w-full h-fit linkDi rounded-3xl flex mx-auto  items-center justify-between overflow-x-auto leading-10 bg-red-60">
                 <Link
                   href={"/development"}
-                  className="w-[300px] h-[400px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                  className="w-[23%] h-[300px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
                 >
                   <div className="w-[100%] h-[100%] relative bg-yellow-00">
                     <div className="w-full h-full flex flex-col justify-between">
                       <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
-                        <div className="w-[170px] h-[170px] bg-blue-40">
+                        <div className="w-[140px] h-[130px] bg-blue-40">
                           <img src={frame3.src} className="w-[100%] h-[100%]" />
                         </div>
                       </div>
                       <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
-                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center z-[10]">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[28px] leading-7 font-[600] text-main-blue text-center z-[10]">
                           Proposal For Development (Scratch)
                         </h1>
                         <div className="h-fit w-[50%] absolute bottom-0 right-0 z-[0]">
@@ -84,17 +84,17 @@ const Home = () => {
                 </Link>
                 <Link
                   href={"/website-upgrading"}
-                  className="w-[300px] h-[400px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                  className="w-[23%] h-[300px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
                 >
                   <div className="w-[100%] h-[100%] relative bg-yellow-00">
                     <div className="w-full h-full flex flex-col justify-between">
                       <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
-                        <div className="w-[170px] h-[170px] bg-blue-40">
+                        <div className="w-[140px] h-[130px] bg-blue-40">
                           <img src={frame3.src} className="w-[100%] h-[100%]" />
                         </div>
                       </div>
                       <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
-                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center z-[10]">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[28px] leading-7 font-[600] text-main-blue text-center z-[10]">
                           Proposal For Development (Upgrading)
                         </h1>
                         <div className="h-fit w-[50%] absolute bottom-0 right-0 z-[0]">
@@ -106,17 +106,17 @@ const Home = () => {
                 </Link>
                 <Link
                   href={"/seo"}
-                  className="w-[300px] h-[400px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                  className="w-[23%] h-[300px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
                 >
                   <div className="w-[100%] h-[100%] relative bg-yellow-00">
                     <div className="w-full h-full flex flex-col justify-between">
                       <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
-                        <div className="w-[170px] h-[170px] bg-blue-40">
+                        <div className="w-[140px] h-[130px] bg-blue-40">
                           <img src={frame1.src} className="w-[100%] h-[100%]" />
                         </div>
                       </div>
                       <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
-                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center z-[10]">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[28px] leading-7 font-[600] text-main-blue text-center z-[10]">
                           Proposal For SEO
                         </h1>
                         <div className="h-fit w-[50%] absolute bottom-0 right-0 z-[0]">
@@ -128,17 +128,17 @@ const Home = () => {
                 </Link>
                 <Link
                   href={"/social-media"}
-                  className="w-[300px] h-[400px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
+                  className="w-[23%] h-[300px] bg-[rgb(251,251,251)] rounded-3xl overflow-hidden border-2 border-color"
                 >
                   <div className="w-[100%] h-[100%] relative bg-yellow-00">
                     <div className="w-full h-full flex flex-col justify-between">
                       <div className="w-[100%] h-[50%] bg-green-40 flex items-end justify-center">
-                        <div className="w-[170px] h-[170px] bg-blue-40">
+                        <div className="w-[140px] h-[130px] bg-blue-40">
                           <img src={frame2.src} className="w-[100%] h-[100%]" />
                         </div>
                       </div>
                       <div className="w-[100%] h-[40%] bg-red-40 flex items-end justify-center">
-                        <h1 className="w-[70%] h-full bg-yellow-5 text-[35px] font-[600] text-main-blue text-center z-[10]">
+                        <h1 className="w-[70%] h-full bg-yellow-5 text-[28px] leading-7 font-[600] text-main-blue text-center z-[10]">
                           Proposal For Social Media
                         </h1>
                         <div className="h-fit w-[50%] absolute bottom-0 right-0 z-[0]">
