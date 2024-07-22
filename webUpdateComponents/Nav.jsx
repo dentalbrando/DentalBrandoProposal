2efbeee2359d76d9f2d977d03eab5934649618ef
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-07-22 11:50:55 +0500

    svsd

--- webUpdateComponents/Nav.jsx
+++ webUpdateComponents/Nav.jsx
@@ -6,25 +6,26 @@ import Link from "next/link";
 const Nav = (props) => {
   // const pageNo = useSelector((state) => state.pageUpdate.formId);
   return (
-    
-      <>
-        <div className="flex justify-start items-center w-full h-[12vh] border-b-2 border-color custom-b relative">
-          <div  className="w-[20vw] flex justify-end items-center z-[10]">
-          <Link href={"/"} className="w-[220px] flex justify-end items-center z-[10]">
+    <>
+      <div className="flex justify-start items-center w-full h-[12vh] border-b-2 border-color custom-b relative bg-red-60">
+        <div className="w-[20vw] flex justify-start items-center z-[10] bg-green-40">
+          <Link
+            href={"/"}
+            className="w-[90%] flex justify-start items-center z-[10] bg-blue-60 ps-8"
+          >
             <img src={logo.src} alt="" className="w-full" />
           </Link>
-          </div>
-          <h1
-            className={`text-main-blue text-[30px] font-medium z-[0]
+        </div>
+        <h1
+          className={`text-main-blue text-[30px] font-medium z-[0]
           ${props.main ? "w-full" : "w-[80vw]"}
           w-[78vw]
-          flex justify-center absolute right-0`}
-          >
-            {props.navText ? props.navText : "Proposal Manager"}
-          </h1>
-        </div>
-      </>
-    
+          flex justify-center absolute right-0 bg-yellow-40`}
+        >
+          {props.navText ? props.navText : "Proposal Manager"}
+        </h1>
+      </div>
+    </>
   );
 };
 
